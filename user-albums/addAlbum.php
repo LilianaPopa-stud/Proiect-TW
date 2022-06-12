60959fcc58b5cdc31d221835d56b79d9d788ab77
Laura Partac <67517427+partaclaura@users.noreply.github.com>
2022-06-12 16:47:40 +0300

    display user albums from db

--- user-albums/addAlbum.php
+++ user-albums/addAlbum.php
@@ -5,7 +5,22 @@ $username = "";
 $album_name = "";
 $errors = array();
 
+$userAlbums = array(); //for user's albums
+
 $db = mysqli_connect('localhost', 'root', '', 'proiect_tw');
+
+//getting user albums
+if(count($errors) == 0){
+    $query = "SELECT album_name FROM albums
+                WHERE username='" . mysqli_real_escape_string($db, $_SESSION['username']) . "'";
+    $result = mysqli_query($db, $query);
+    if(mysqli_num_rows($result) > 0){
+        while($album = mysqli_fetch_assoc($result)){
+            array_push($userAlbums, $album['album_name']);
+        }
+    }
+    $_SESSION['albums'] = $userAlbums;  
+}
 
 if(isset($_POST['add_album'])){
 
