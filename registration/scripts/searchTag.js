734165358ea4f5bb369b9eba8bebaa37025c924b
Laura Partac <67517427+partaclaura@users.noreply.github.com>
2022-06-19 19:39:28 +0300

    details

--- registration/scripts/searchTag.js
+++ registration/scripts/searchTag.js
@@ -1,5 +1,5 @@
 function Redirect(){
     let input = document.getElementById('searchbar').value;
     //input = input.lowerCase;
-    window.location = "../social/tags/filterPage.php?name=".concat(input);
+    window.location = "social/tags/filterPage.php?name=".concat(input);
 }
